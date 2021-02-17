0ab872da34bc3745943bbbc47f519e74a7eccf54
SkrrtNick <nickadamjohnwest+skrrtnick@gmail.com>
2021-02-17 23:56:26 +0000

    Initial commit

--- src/com/skrrtnick/cutnburn/SkrrtCutnBurn.java
+++ src/com/skrrtnick/cutnburn/SkrrtCutnBurn.java
@@ -1,15 +1,97 @@
 package com.skrrtnick.cutnburn;
 
+import com.epicbot.api.os.model.game.GameState;
+import com.epicbot.api.shared.APIContext;
+import com.epicbot.api.shared.GameType;
 import com.epicbot.api.shared.script.LoopScript;
+import com.epicbot.api.shared.script.ScriptManifest;
+import com.epicbot.api.shared.script.TreeScript;
+import com.epicbot.api.shared.script.tree.BranchTask;
+import com.epicbot.api.shared.script.tree.LeafTask;
+import com.epicbot.api.shared.script.tree.TreeTask;
+import com.epicbot.api.shared.util.Random;
+import com.skrrtnick.cutnburn.leafs.login;
+import com.skrrtnick.cutnburn.leafs.walkToArea;
 
-public class SkrrtCutnBurn extends LoopScript {
+//public class SkrrtCutnBurn extends LoopScript {
+//    @Override
+//    protected int loop() {
+//        return 800;
+//    }
+//
+//    @Override
+//    public boolean onStart(String... strings) {
+//        return false;
+//    }
+//}
+@ScriptManifest(name = "Skrrt Cut n Burn", gameType = GameType.OS)
+public class SkrrtCutnBurn extends TreeScript
+{
     @Override
-    protected int loop() {
-        return 800;
+    public boolean onStart(String... args)
+    {
+        Branch rootBranch = new Branch(getAPIContext(), "Root")
+        {
+            @Override
+            protected TreeTask createSuccessTask(APIContext ctx)
+            {
+                return new Branch(getAPIContext(), "A");
+            }
+
+            @Override
+            protected TreeTask createFailureTask(APIContext ctx)
+            {
+                return new Branch(getAPIContext(), "B");
+            }
+        };
+        setRootTask(rootBranch);
+
+        return true;
     }
 
-    @Override
-    public boolean onStart(String... strings) {
-        return false;
+    private class Branch extends BranchTask
+    {
+        private String name;
+
+        public Branch(APIContext ctx, String name)
+        {
+            super(ctx);
+            this.name = name;
+        }
+
+        @Override
+        protected TreeTask createSuccessTask(APIContext ctx)
+        {
+            return new walkToArea(ctx, name + " -> Success");
+        }
+
+        @Override
+        protected TreeTask createFailureTask(APIContext ctx)
+        {
+            return new login(ctx, name + " -> Failure");
+        }
+
+        @Override
+        public boolean validate()
+        {
+            return ctx.game().getGameState().equals(GameState.LOGGED_IN);
+        }
+    }
+
+    private class Leaf extends LeafTask
+    {
+        private String name;
+
+        public Leaf(APIContext ctx, String name)
+        {
+            super(ctx);
+            this.name = name;
+        }
+
+        @Override
+        public void execute()
+        {
+            System.out.println("Execute " + name);
+        }
     }
 }
---
+++ src/com/skrrtnick/cutnburn/data/Tree.java
@@ -0,0 +1,52 @@
+package com.skrrtnick.cutnburn.data;
+
+import com.epicbot.api.shared.model.Area;
+
+public enum Tree {
+    TREE("Tree", Location.TREE_AREA.getArea(),1,1),
+    OAK("Oak", Location.OAKS_AREA.getArea(),15,15),
+    WILLOW("Willow", Location.WILLOWS_AREA.getArea(),30,15);
+//    YEW("Yew", new Area(new Tile(3092, 3245, 0), new Tile(3094, 3240, 0)));
+
+    private Area area;
+    private String name;
+    private int wcRequiredLevel;
+    private int fmRequiredLevel;
+
+    public String getName() {
+        return name;
+    }
+
+    Tree(String name, Area area, int wcRequiredLevel, int fmRequiredLevel) {
+        this.name = name;
+        this.area = area;
+        this.wcRequiredLevel = wcRequiredLevel;
+        this.fmRequiredLevel = fmRequiredLevel;
+    }
+
+    public Area getArea() {
+        return area;
+    }
+
+    public int getFmRequiredLevel() {
+        return fmRequiredLevel;
+    }
+
+    public int getWcRequiredLevel() {
+        return wcRequiredLevel;
+    }
+
+    public static Tree getBestTree(int fm, int wc) {
+        Tree temp = null;
+        for (Tree t : Tree.values()) {
+            if (t.getFmRequiredLevel() <= fm && t.getWcRequiredLevel() <= wc) {
+                temp = t;
+            } else {
+                return temp;
+            }
+        } return temp;
+
+    }
+
+
+}
